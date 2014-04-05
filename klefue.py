7e6d1b02d1157fb061d2d815bbfc74de64886c79
5k41 <mail@p-mueller.net>
2014-04-05 23:56:52 +0200

    Added interactive support to find routes.

--- klefue.py
+++ klefue.py
@@ -6,26 +6,55 @@
 
     Download of the database from
     http://db-sandsteinklettern.gipfelbuch.de/download
+    (update ("aktualisieren") the database for .klf
+     "Datei klefue.klf aktualisieren [click]"      )
     http://db-sandsteinklettern.gipfelbuch.de/daten/klefue.klf
 
     TODO:
     deal with gipfelname_de/gipfelname_cz
 """
-
+from __future__ import print_function
 
 class Klefue(object):
     def __init__(self, klefuefile, encoding="windows-1252" ):
-        f = open(klefuefile, 'r')
+        f = open(klefuefile, 'r', encoding=encoding)
         data = f.readlines()
         index = list()
         for i in range(len(data)):
-            line = data[i]
-            data[i] = line.decode(encoding)
-            if line[0] == "$":
-                index.append([i, line.strip().strip("$:").lower(),
+            #line = data[i]
+            #data[i] = line.decode(encoding)
+            if data[i][0] == "$":
+                index.append([i, data[i].strip().strip("$:").lower(),
                              data[i+1].lower().strip().split("\t")])
         self.index = index
         self.rawdata = data
+        
+        # Create dictionaries for regions and sectors, and peaks
+        # Gebiet
+        self.regions = dict()
+        for i in range(self.index[0][0]+2,self.index[1][0]-1):
+            line = self.rawdata[i]
+            a = line.split("\t")
+            a[0] = int(a[0])
+            self.regions[a[0]] = a
+
+        # Teilgebiet
+        self.sectors = dict()
+        for i in range(self.index[1][0]+2,self.index[2][0]-1):
+            line = self.rawdata[i]
+            a = line.split("\t")
+            a[0] = int(a[0])
+            a[1] = int(a[1])
+            self.sectors[a[1]] = a
+
+        # Gipfel
+        self.peaks = dict()
+        for i in range(self.index[2][0]+2,self.index[3][0]-1):
+            line = self.rawdata[i]
+            a = line.split("\t")
+            a[0] = int(a[0])
+            a[1] = int(a[1])
+            self.peaks[a[1]] = a
 
     def get_header(self, tablename):
         """ Returns the header data of the klefue tables.
@@ -69,20 +98,26 @@ class Klefue(object):
         results = dict()
         for item in self.index:
             results[item[1]] = list()
+
         for key in searchdata.keys():
-            for j in range(len(self.index)):
+            for j in range(len(self.index)-1):
                 if key > self.index[j][0] and key < self.index[j+1][0]:
                     results[self.index[j][1]].append(searchdata[key].split("\t"))
+            if key > self.index[j][0]:
+                results[self.index[j][1]].append(searchdata[key].split("\t"))
+
         if typeres is not None:
             typeres = typeres.strip()
             return results[typeres.lower()]
         else:
             return results
 
+
     def search_peak(self, word):
         """ Find all peaks that have word in their name.
         """
         return self.search(word, typeres="gipfel")
+
 
     def get_peak_routes(self, peakid):
         """ Using the identifier of a peak, get the list of routes.
@@ -96,21 +131,91 @@ class Klefue(object):
             except:
                 pass
         return results
+        
+        
+    def get_peak_region_and_sector_names(self, peakid):
+        """ Using the identifier of a peak, get the region
+            and area of that peak.
+        """
+        secid = self.get_sector_id_from_peak_id(peakid)
+        sector = self.sectors[secid]
+        regid = self.get_region_id_from_peak_id(peakid)
+        region = self.regions[regid]
+        return [region[1], sector[2]]
+
+
+    def get_region_id_from_peak_id(self, peakid):
+        # get sector
+        secid = self.peaks[peakid][0]
+        sector = self.sectors[secid]
+        # get region
+        return sector[0]
+        
+        
+    def get_sector_id_from_peak_id(self, peakid):
+        # get sector
+        secid = self.peaks[peakid][0]
+        # get region
+        return secid
+        
 
     def get_peak_id(self, proplist):
         """ Using the list that we create, when we call search_peak,
             return the peak id.
         """
         # Check with get_header("gipfel")
-        return proplist[1]
+        return proplist[2]
 
 if __name__ == "__main__":
-    klefuefile = "klefue.kf3"
-    klefue = Klefue(klefuefile)
-    a=klefue.search("hunskirche", "gipfel")
-    columns = ["gipfelname_d", "gipfelid", "gipfelnord", "gipfelost"]
+    klefuefile = "klefue.klf"
+    KF = Klefue(klefuefile)
+    route_columns = ["WEGNAME_D", "SCHWIERIGKEIT", "WEGNR",
+                     "WEGBESCHREIBUNG_D"]
+    peak_columns = ["gipfelname_d", "gipfelid", "gipfelnord",
+                    "gipfelost"]
+    while True:
+        print("What do you want to search for? \n"+
+              "1. Peak (Gipfel) \n"+
+              "2. Route (Weg) \n")
+        choice = input()
+        # Check for valid choice
+        if choice == "1":
+            stype = "gipfel"
+            columns = peak_columns
+            break
+        elif choice == "2":
+            stype = "wege"
+            columns = route_columns
+            break
+    print("Type a name.\n")
+    search = input()
+    
+    a=KF.search(search, stype)
+    a.sort()
 
+    if stype == "gipfel":
+        print("For which peak do you want to display the routes?")
+        printdata = list()
+        for i in range(len(a)):
+            nam = KF.get_peak_region_and_sector_names(int(a[i][1]))
+            printdata.append(nam)
+        printdata.sort()
+        
+        for i in range(len(a)):
+            nam = printdata[i]
+            print("{}. {} - {} - {}".
+                  format(i, nam[0], nam[1], a[i][2]))
+        q = int(input())
+        if q in range(len(a)):
+            a = KF.get_peak_routes(a[q][1])
+        columns = route_columns
+        stype = "wege"
+        
+    b=KF.get_columns(a, columns, stype)
+    b.sort()
+    for i in range(len(b)):
+        print("{} \t {} \t {} \t {}".
+              format(b[i][0], b[i][1], b[i][2], b[i][3]))
 
-    b=klefue.get_columns(a, columns, "gipfel")
-
-    print b
+    import IPython
+    IPython.embed()
